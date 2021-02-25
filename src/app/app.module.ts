c165f4b305aac19e846abdc64ce2e7bce1453e49
TakeYouWanna <stepankovarty@gmail.com>
2021-02-25 20:16:57 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
+import { AngularFireModule } from '@angular/fire'
 import { AppRoutingModule } from './app-routing.module'
+
 import { AppComponent } from './app.component'
+import { TopBarComponent } from './modules/top-bar/top-bar.component'
+import { LoginWindowComponent } from './modules/login-window/login-window.component'
+import { RegistrWindowComponent } from './modules/registr-window/registr-window.component'
 
 @NgModule({
-  declarations: [AppComponent],
-
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    LoginWindowComponent,
+    RegistrWindowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyAsaJIGqvhCaxlUDDmyXqSjKNArwaT6SXM',
+      authDomain: 'innowisepracticelevel1.firebaseapp.com',
+      projectId: 'innowisepracticelevel1',
+      storageBucket: 'innowisepracticelevel1.appspot.com',
+      messagingSenderId: '678231746401',
+      appId: '1:678231746401:web:9bfd49416d71d73844c720',
+      measurementId: 'G-LX1GLSW2SN'
+    })],
   providers: [],
   bootstrap: [AppComponent]
 })
