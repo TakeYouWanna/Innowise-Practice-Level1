8e5123ae98fcd020f21400210d0ceff77b60628e
TakeYouWanna <stepankovarty@gmail.com>
2021-02-25 20:16:12 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { LoginWindowComponent } from './modules/login-window/login-window.component'
+import { RegistrWindowComponent } from './modules/registr-window/registr-window.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: 'authorization', component: LoginWindowComponent },
+  { path: 'registration', component: RegistrWindowComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
