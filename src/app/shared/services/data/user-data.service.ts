554d5fe77037c5c46e67a3c2ed37f2c444249301
TakeYouWanna <stepankovarty@gmail.com>
2021-03-05 21:34:02 +0300

    Update user-data.service

--- src/app/shared/services/data/user-data.service.ts
+++ src/app/shared/services/data/user-data.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
+import { User } from '../../interfaces/user'
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class UserDataService {
-  public user$: BehaviorSubject<string> = new BehaviorSubject<string>('')
+  public user$: BehaviorSubject<User> = new BehaviorSubject<User>({
+    uid: '',
+    email: ''
+  })
 
-  public setUser (data: string): void {
-    this.user$.next(JSON.parse(data).email)
+  constructor () {
+    const user = localStorage.getItem('user')
+    if (user != null) {
+      this.user$.next(JSON.parse(user))
+    }
   }
 
-  public deleteUser (): void {
-    this.user$.next('')
+  public setUser (user: User): void {
+    this.user$.next(user)
+    localStorage.setItem('user', JSON.stringify(user))
+  }
+
+  public deleteuser (): void {
+    this.user$.next({ uid: '', email: '' })
+    localStorage.removeItem('user')
   }
 }
